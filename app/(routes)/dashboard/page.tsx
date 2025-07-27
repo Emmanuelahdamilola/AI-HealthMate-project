fa6694be885723e5326b37f1767ac89e5c1a31bf
Emmanuelahdamilola <emmanuelahdamilola1992@gmail.com>
2025-07-27 01:24:09 +0100

    error fixed

--- app/(routes)/dashboard/page.tsx
+++ app/(routes)/dashboard/page.tsx
@@ -6,7 +6,7 @@ export default function Dashboard() {
   return (
     <div>
       <div className='flex items-center justify-between'>
-        <h2>My Dashboard</h2>
+        <h2 className='mb-50'>My Dashboard</h2>
        <AddNewSessionDialog/>
       </div>
       <UserHistory />
