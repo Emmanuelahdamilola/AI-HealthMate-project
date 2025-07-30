0dfaf6dd55b2ad2250be81c0885709da7929ee4a
Emmanuelahdamilola <emmanuelahdamilola1992@gmail.com>
2025-07-30 08:32:27 +0100

    Doctor fixed

--- app/(routes)/dashboard/_components/Doctors.tsx
+++ app/(routes)/dashboard/_components/Doctors.tsx
@@ -4,9 +4,7 @@ import AiDoctorAgentCard from './AiDoctorAgentCard'
 
 export default function AIDoctors() {
   return (
-    <div className="mt-5 px-4">
-      <p className="text-center text-lg italic mb-5">Select a doctor to get started</p>
-
+    <div className="mt-10 px-4 ">
       <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6">
         {AiDoctorList.map((doctor, index) => (
           <AiDoctorAgentCard key={index} AiDoctorAgent={doctor} />
--- app/(routes)/dashboard/doctors/page.tsx
+++ app/(routes)/dashboard/doctors/page.tsx
@@ -4,7 +4,7 @@ import Doctors from '../_components/Doctors'
 export default function DoctorsPage() {
   return (
     <div>
-      <h2 className="text-xl font-semibold mb-4">Meet Our AI Medical Specialists</h2>
+      <h2 className="text-xl font-semibold mb-4 text-center">Meet Our AI Medical Specialists</h2>
       <Doctors />
     </div>
   )
